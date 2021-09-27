0904200de90ab8d26ec1191b74aaf063a52d1145
Andre Silva <silva.andre.daniel@gmail.com>
2021-09-27 14:54:59 +0100

    AMB-848 adding validfation flag to headers

--- docker/Transformation-Engine/src/main/java/nhsd/fhir/transformationenginepoc/controller/ConversionController.java
+++ docker/Transformation-Engine/src/main/java/nhsd/fhir/transformationenginepoc/controller/ConversionController.java
@@ -1,10 +1,15 @@
 package nhsd.fhir.transformationenginepoc.controller;
 
+import ca.uhn.fhir.validation.SingleValidationMessage;
+import ca.uhn.fhir.validation.ValidationResult;
 import nhsd.fhir.transformationenginepoc.service.FileConversionService;
+import nhsd.fhir.transformationenginepoc.service.ValidationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value = "/convert")
@@ -14,16 +19,30 @@ public class ConversionController {
     @Autowired
     private FileConversionService fileConversionService;
 
+    @Autowired
+    private ValidationService validationService;
+
     @PostMapping(consumes = MediaType.ALL_VALUE)
     public ResponseEntity<?> convert(@RequestHeader("Content-Type") final String content_type,
                                      @RequestHeader("Accept") final String accept,
+                                     @RequestHeader("Validate") final Boolean toValidate,
                                      @RequestBody final String fhirSchema) {
-
 
         final String currentVersion = content_type.split(";")[1].split("=")[1];
         final String targetVersion = accept.split(";")[1].split("=")[1];
         final MediaType mediaTypeIn = content_type.split(";")[0].contains("xml") ? MediaType.APPLICATION_XML : MediaType.APPLICATION_JSON;
         final MediaType mediaTypeInOut = accept.split(";")[0].contains("xml") ? MediaType.APPLICATION_XML : MediaType.APPLICATION_JSON;
+
+        if (toValidate) {
+            final ValidationResult checkedInputFile = validationService.validateSchema(currentVersion, fhirSchema);
+
+            if (!checkedInputFile.isSuccessful()) {
+                return ResponseEntity.unprocessableEntity().body(checkedInputFile.getMessages()
+                    .stream()
+                    .map(SingleValidationMessage::getMessage)
+                    .collect(Collectors.joining(",")));
+            }
+        }
 
         final String convertedFhir = fileConversionService.convertFhirSchema(currentVersion, targetVersion, mediaTypeIn, mediaTypeInOut, fhirSchema);
 
