91b74ac00bcdfa10e4c20b256a3e86f713e65ec0
Jalal Hosseini <jalalhosseiny@gmail.com>
2021-10-05 14:44:42 +0100

    AMB-00 add retries to /_status

--- tests/test_deployment.py
+++ tests/test_deployment.py
@@ -1,5 +1,7 @@
 import pytest
 import requests
+from requests.adapters import HTTPAdapter
+from urllib3.util.retry import Retry
 
 
 @pytest.mark.run(order=1)
@@ -17,7 +19,15 @@ class TestDeployment:
         assert res.status_code == 200
 
     def test_status(self, proxy_url, commit_id, cmd_options):
-        res = requests.get(f"{proxy_url}/_status", headers={"apikey": cmd_options["--status-api-key"]})
+        request = requests.Session()
+        retries = Retry(total=10,
+                        # {backoff factor} * (2 ** ({number of total retries} - 1)) -> almost 30 seconds
+                        backoff_factor=0.03,
+                        status_forcelist=[500, 502, 503, 504])
+
+        request.mount('https://', HTTPAdapter(max_retries=retries))
+
+        res = request.get(f"{proxy_url}/_status", headers={"apikey": cmd_options["--status-api-key"]})
 
         assert res.status_code == 200
         assert res.json()["commitId"] == commit_id
