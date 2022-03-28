05393800311c65dff1cce1c4d8dc99db4456d047
Jalal Hosseini <jalalhosseiny@gmail.com>
2022-03-28 15:13:46 +0100

    AMB-1028 revert AllergyIntolerance coding extension transformer.

--- docker/Transformation-Engine/src/main/kotlin/net/nhsd/fhir/converter/transformer/CareconnectTransformer.kt
+++ docker/Transformation-Engine/src/main/kotlin/net/nhsd/fhir/converter/transformer/CareconnectTransformer.kt
@@ -2,7 +2,6 @@ package net.nhsd.fhir.converter.transformer
 
 import org.hl7.fhir.instance.model.api.IBaseResource
 import org.springframework.stereotype.Component
-import org.hl7.fhir.dstu3.model.AllergyIntolerance as R3AllergyIntolerance
 import org.hl7.fhir.dstu3.model.DomainResource as R3Resource
 import org.hl7.fhir.dstu3.model.Extension as R3Extension
 import org.hl7.fhir.r4.model.DomainResource as R4Resource
@@ -13,23 +12,17 @@ typealias ExtensionTransformer = (src: R3Extension, tgt: R4Resource) -> Unit
 class CareconnectTransformer(private val extensionsMap: HashMap<String, ExtensionTransformer> = careconnectTransformers) :
     Transformer {
 
-    override fun transform(source: IBaseResource, target: IBaseResource): IBaseResource {
-        return if (source is R3Resource && target is R4Resource) {
-            applyTransformers(source.extension, target)
-            if (source is R3AllergyIntolerance) {
-                applyTransformers(source.code.extension, target)
-            }
-            target
+    override fun transform(src: IBaseResource, tgt: IBaseResource): IBaseResource {
+        return if (src is R3Resource && tgt is R4Resource) {
+            src.extension
+                .filter { extensionsMap.containsKey(it.url) }
+                .also { tgt.extension.removeIf { extensionsMap.containsKey(it.url) } }
+                .forEach { extensionsMap[it.url]?.invoke(it, tgt) }
+
+            tgt
 
         } else {
-            target
+            tgt
         }
-    }
-
-    private fun applyTransformers(extensions: List<R3Extension>, target: R4Resource) {
-        extensions
-            .filter { extensionsMap.containsKey(it.url) }
-            .also { target.extension.removeIf { extensionsMap.containsKey(it.url) } }
-            .forEach { extensionsMap[it.url]?.invoke(it, target) }
     }
 }
