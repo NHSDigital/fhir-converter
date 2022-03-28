e39b6a6abbb8ecade4262f7870b2322f8ce6b121
Jalal Hosseini <jalalhosseiny@gmail.com>
2022-03-28 16:17:07 +0100

    AMB-1028 handle extensions that are inside AllergyIntolerance.code.coding[*] with descriptionId and descriptionDisplay

--- docker/Transformation-Engine/src/main/kotlin/net/nhsd/fhir/converter/transformer/CareconnectTransformer.kt
+++ docker/Transformation-Engine/src/main/kotlin/net/nhsd/fhir/converter/transformer/CareconnectTransformer.kt
@@ -1,9 +1,12 @@
 package net.nhsd.fhir.converter.transformer
 
+import org.hl7.fhir.dstu3.model.AllergyIntolerance
 import org.hl7.fhir.instance.model.api.IBaseResource
 import org.springframework.stereotype.Component
+import org.hl7.fhir.dstu3.model.AllergyIntolerance as R3AllergyIntolerance
 import org.hl7.fhir.dstu3.model.DomainResource as R3Resource
 import org.hl7.fhir.dstu3.model.Extension as R3Extension
+import org.hl7.fhir.r4.model.AllergyIntolerance as R4AllergyIntolerance
 import org.hl7.fhir.r4.model.DomainResource as R4Resource
 
 typealias ExtensionTransformer = (src: R3Extension, tgt: R4Resource) -> Unit
@@ -12,17 +15,34 @@ typealias ExtensionTransformer = (src: R3Extension, tgt: R4Resource) -> Unit
 class CareconnectTransformer(private val extensionsMap: HashMap<String, ExtensionTransformer> = careconnectTransformers) :
     Transformer {
 
-    override fun transform(src: IBaseResource, tgt: IBaseResource): IBaseResource {
-        return if (src is R3Resource && tgt is R4Resource) {
-            src.extension
+    override fun transform(source: IBaseResource, target: IBaseResource): IBaseResource {
+        return if (source is R3Resource && target is R4Resource) {
+            source.extension
                 .filter { extensionsMap.containsKey(it.url) }
-                .also { tgt.extension.removeIf { extensionsMap.containsKey(it.url) } }
-                .forEach { extensionsMap[it.url]?.invoke(it, tgt) }
+                .also { target.extension.removeIf { extensionsMap.containsKey(it.url) } }
+                .forEach { extensionsMap[it.url]?.invoke(it, target) }
 
-            tgt
+            if (source is AllergyIntolerance) {
+                handleAllergyIntolerance(source, target as R4AllergyIntolerance)
+            }
+
+            target
 
         } else {
-            tgt
+            target
         }
+    }
+
+    fun handleAllergyIntolerance(source: R3AllergyIntolerance, target: R4AllergyIntolerance) {
+        // Handle code.coding[*].extension with descriptionId and descriptionDisplay
+        source.code?.coding?.forEachIndexed { index, coding ->
+            val r3Ext = coding.getExtensionsByUrl(CARECONNECT_DESCRIPTION_ID_URL)?.firstOrNull()
+            if (r3Ext != null) {
+                val r4Ext = descriptionIdAndDisplay(r3Ext)
+                target.code.coding[index].addExtension(r4Ext)
+            }
+        }
+        // Remove uccore extensions that are carried over by base converter
+        target.code?.coding?.forEach { coding -> coding.extension.removeIf { it.url == CARECONNECT_DESCRIPTION_ID_URL } }
     }
 }
---
+++ docker/Transformation-Engine/src/test/kotlin/net/nhsd/fhir/converter/transformer/allergy_intolerance_coding_test.kt
@@ -0,0 +1,89 @@
+package net.nhsd.fhir.converter.transformer
+
+import ca.uhn.fhir.context.FhirVersionEnum
+import net.javacrumbs.jsonunit.assertj.JsonAssert
+import org.assertj.core.api.Assertions.assertThat
+import org.hl7.fhir.r4.model.IdType
+import org.hl7.fhir.r4.model.StringType
+import org.junit.jupiter.api.Test
+import org.springframework.http.MediaType
+import org.hl7.fhir.dstu3.model.AllergyIntolerance as R3AllergyIntolerance
+import org.hl7.fhir.dstu3.model.CodeableConcept as R3CodeableConcept
+import org.hl7.fhir.dstu3.model.Coding as R3Coding
+import org.hl7.fhir.dstu3.model.Extension as R3Extension
+import org.hl7.fhir.dstu3.model.IdType as R3IdType
+import org.hl7.fhir.r4.model.AllergyIntolerance as R4AllergyIntolerance
+import org.hl7.fhir.r4.model.CodeableConcept as R4CodeableConcept
+import org.hl7.fhir.r4.model.Coding as R4Coding
+
+internal class AllergyIntoleranceCodingTest {
+    @Test
+    internal fun `it should handle extension inside AllergyIntolerance coding`() {
+        // Given
+        val desId = R3Extension("descriptionId", R3IdType("descriptionId value"))
+        val desDisplay = R3Extension("descriptionDisplay", R3IdType("descriptionDisplay value"))
+        val r3Extension = R3Extension(CARECONNECT_DESCRIPTION_ID_URL).apply {
+            addExtension(desId)
+            addExtension(desDisplay)
+        }
+
+        // A Resource can have multiple codings
+        val source = R3AllergyIntolerance().apply {
+            code = R3CodeableConcept().apply {
+                val code1 = R3Coding().apply { addExtension(r3Extension) }
+                val code2 = R3Coding().apply { addExtension(r3Extension) }
+                coding = listOf(code1, code2)
+            }
+        }
+
+        val target = R4AllergyIntolerance().apply {
+            code = R4CodeableConcept().apply {
+                val code1 = R4Coding()
+                val code2 = R4Coding()
+                coding = listOf(code1, code2)
+            }
+        }
+
+        val transformer = CareconnectTransformer()
+
+        // When
+        val expectedResource = transformer.transform(source, target) as R4AllergyIntolerance
+
+        // Then
+        expectedResource.code.coding.forEach { coding ->
+            assertThat(coding.getExtensionByUrl(CARECONNECT_DESCRIPTION_ID_URL)).isNull()
+        }
+
+        expectedResource.code.coding.forEach { coding ->
+            val actualExtension = coding.getExtensionByUrl(UKCORE_DESCRIPTION_ID_URL)
+
+            val r3DesId = actualExtension.getExtensionByUrl("descriptionId")
+            assertThat(r3DesId.url).isEqualTo("descriptionId")
+            assertThat(r3DesId.value).isInstanceOf(IdType::class.java)
+            assertThat(r3DesId.value.primitiveValue()).isEqualTo("descriptionId value")
+
+            val r3DesDisplay = actualExtension.getExtensionByUrl("descriptionDisplay")
+            assertThat(r3DesDisplay.url).isEqualTo("descriptionDisplay")
+            assertThat(r3DesDisplay.value).isInstanceOf(StringType::class.java)
+            assertThat(r3DesDisplay.value.primitiveValue()).isEqualTo("descriptionDisplay value")
+        }
+    }
+
+    @Test
+    internal fun `it should handle IOPS derived examples`() {
+        val input = loadExtraExample("extra-examples/expected/AllergyIntoleranceExtensionId-Extension-3to4_000.json")
+        val expected =
+            loadExtraExample("extra-examples/expected/AllergyIntoleranceExtensionId-Extension-3to4_000.json")
+        val converterService = makeConverterService()
+
+        val actualResource = converterService.convert(
+            input,
+            MediaType.APPLICATION_JSON,
+            FhirVersionEnum.DSTU3,
+            MediaType.APPLICATION_JSON,
+            FhirVersionEnum.R4
+        )
+
+        JsonAssert.assertThatJson(actualResource).isEqualTo(expected)
+    }
+}
