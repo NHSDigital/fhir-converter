e8aec7e34a2e1cfb8159a227bb49d8d616369abb
Jalal Hosseini <jalalhosseiny@gmail.com>
2022-04-01 14:05:52 +0100

    AMB-1061 move MedicationStatement ChangeSummary

--- docker/Transformation-Engine/src/test/kotlin/net/nhsd/fhir/converter/transformer/changeSummary_test.kt
+++ docker/Transformation-Engine/src/test/kotlin/net/nhsd/fhir/converter/transformer/changeSummary_test.kt
@@ -4,15 +4,14 @@ import ca.uhn.fhir.context.FhirVersionEnum
 import net.javacrumbs.jsonunit.assertj.JsonAssert
 import org.assertj.core.api.Assertions
 import org.hl7.fhir.dstu3.model.MedicationStatement
-import org.junit.jupiter.api.Test
-import org.hl7.fhir.dstu3.model.Extension as R3Extension
-import org.hl7.fhir.r4.model.MedicationStatement as R4MedicationStatement
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.Arguments
 import org.junit.jupiter.params.provider.MethodSource
 import org.junit.jupiter.params.provider.ValueSource
 import org.springframework.http.MediaType
 import java.util.stream.Stream
+import org.hl7.fhir.dstu3.model.Extension as R3Extension
+import org.hl7.fhir.r4.model.MedicationStatement as R4MedicationStatement
 
 internal class ChangeSummaryTest {
     @ParameterizedTest
@@ -34,11 +33,9 @@ internal class ChangeSummaryTest {
 
     }
 
-    @Test
-    internal fun `it should handle IOPS derived examples`() {
-        val input = loadExtraExample("extra-examples/input/MedicationStatementChangeSummary-Extension-3to4_000.json")
-        val expected =
-            loadExtraExample("extra-examples/expected/MedicationStatementChangeSummary-Extension-3to4_000.json")
+    @ParameterizedTest(name = "Test MedicationStatement changeSummary extensions with IOPS example index: {index} (zero based indexing)")
+    @MethodSource("provideExamples")
+    internal fun `it should handle IOPS examples`(input: String, expected: String) {
         val converterService = makeConverterService()
 
         val actualResource = converterService.convert(
@@ -50,5 +47,16 @@ internal class ChangeSummaryTest {
         )
 
         JsonAssert.assertThatJson(actualResource).isEqualTo(expected)
+    }
+
+    companion object {
+        @JvmStatic
+        fun provideExamples(): Stream<Arguments> {
+            val careconnectExampleLoader = CareconnectExampleLoader()
+            val pairs = careconnectExampleLoader.loadExample(MedicationStatement::class.java, "ChangeSummary")
+                .map { Arguments.of(it.input, it.output) }
+
+            return pairs.stream()
+        }
     }
 }
