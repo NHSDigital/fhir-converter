6f60159fec7cb74991e2487df9053c874c4c63da
Ely Doodson <ely.doodson@airelogic.com>
2022-05-10 16:00:53 +0100

    added missing json option

--- docker/Transformation-Engine/build.gradle.kts
+++ docker/Transformation-Engine/build.gradle.kts
@@ -24,7 +24,7 @@ dependencies {
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 	implementation("ca.uhn.hapi.fhir:hapi-fhir-converter:${hapiVersion}")
-    implementation("ca.uhn.hapi.fhir:org.hl7.fhir.convertors:5.6.45")
+    implementation("ca.uhn.hapi.fhir:org.hl7.fhir.convertors:5.6.45") 
 	implementation("ca.uhn.hapi.fhir:hapi-fhir-structures-dstu3:${hapiVersion}")
 	implementation("ca.uhn.hapi.fhir:hapi-fhir-structures-r4:${hapiVersion}")
 	testImplementation("org.springframework.boot:spring-boot-starter-test:${springVersion}")
